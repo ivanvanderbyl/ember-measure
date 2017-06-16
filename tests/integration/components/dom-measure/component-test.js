24d426cd09c3e18c51c6ba37d0c51a1ba81eacf5
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2017-06-16 16:04:54 +0200

    Cleaned up

--- tests/integration/components/dom-measure/component-test.js
+++ tests/integration/components/dom-measure/component-test.js
@@ -5,21 +5,42 @@ moduleForComponent('dom-measure', 'Integration | Component | dom measure', {
   integration: true
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{dom-measure}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
+test('it renders and has measurement on first render', function(assert) {
   this.render(hbs`
-    {{#dom-measure}}
-      template block text
-    {{/dom-measure}}
+    <div style="width: 100px;height:100px;magrin:0;padding:0;line-height:16px;">
+      {{#dom-measure as |rect|}}
+        <p>Hello World</p>
+        {{display-content-rect rect=rect}}
+      {{/dom-measure}}
+    </div>
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  let rect = JSON.parse(this.$('pre').text().trim());
+
+  assert.ok(rect.top, 'top');
+  assert.ok(rect.right, 'right');
+  assert.ok(rect.bottom, 'bottom');
+  assert.ok(rect.left, 'left');
+  assert.ok(rect.width, 'width');
+  assert.equal(rect.height, 8, 'height');
+});
+
+test('it measures SVG tags correctly', function(assert) {
+  this.render(hbs`
+    <div style="width: 100px;height:100px;magrin:0;padding:0;line-height:16px;">
+      {{#dom-measure tagName="svg" as |rect|}}
+        <text>Hello World</text>
+        <g>{{display-content-rect tagName="text" rect=rect}}</g>
+      {{/dom-measure}}
+    </div>
+  `);
+
+  let rect = JSON.parse(this.$('g text').text().trim());
+
+  assert.ok(rect.top, 'top');
+  assert.ok(rect.right, 'right');
+  assert.ok(rect.bottom, 'bottom');
+  assert.ok(rect.left, 'left');
+  assert.ok(rect.width, 'width');
+  assert.ok(rect.height, 'height');
 });
